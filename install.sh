ff718a98ca2ed8b2db867fff9bc6efc2649fcf3e
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-01-11 09:27:16 +0000

    install script for build

---
+++ install.sh
@@ -0,0 +1,37 @@
+## Installer Script
+# github
+sudo apt-get install git-all
+# snap
+sudo apt-get install snap
+# repo
+snap install repo
+# python3.11
+sudo apt-get install python3.11
+# python-is-python3
+sudo apt-get install python-is-python3
+# open jdk
+sudo apt-get install openjdk-11-jdk
+# unzip
+sudo apt-get install unzip
+# visual studio code
+snap install code --classic
+# gradle
+snap install gradle --classic
+# curl
+snap install curl
+# pip3
+sudo apt-get install python3-pip
+#lunch
+pip3 install lunch
+# github desktop (unofficial)
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.2.0-linux1/GitHubDesktop-linux-3.2.0-linux1.deb
+# install
+sudo apt-get install gdebi
+# install 2
+sudo gdebi GitHubDesktop-linux-3.2.0-linux1.deb
+# flatpak
+sudo apt-get install flatpak
+# chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# chrome install
+sudo dpkg -i google-chrome-stable_current_amd64.deb
