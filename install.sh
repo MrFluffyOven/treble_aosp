bfdf9bcaf840792fd46c681d9bfa8ce9efcd6f3d
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-01-11 15:05:23 +0000

    Cleanup

--- install.sh
+++ install.sh
@@ -7,48 +7,18 @@ echo
 
 
 Snap&Curl() {
-echo "--> Installing Snap" 
+echo "--> Installing Snap & Curl" 
 sudo apt-get install snap
-echo
-
-echo "--> Installing Curl"
 snap install curl
 echo 
 }  
 
 
-Tools() {
-echo "--> Installing Unzip" 
-sudo apt-get install unzip
-echo
-
-echo "--> Installing Visual Studio Code" 
-snap install code --classic
-echo
-
-echo "--> Installing Gdebi" 
-sudo apt-get install gdebi
-echo
-
-echo "--> Installing Google Chrome" 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-echo
-}   
-
-
 Git&Repo() {
-echo "--> Installing Git" 
-sudo apt-get install git-all 
-echo
-
-echo "--> Installing Github Desktop (Unofficial)" 
-sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.2.0-linux1/GitHubDesktop-linux-3.2.0-linux1.deb
-sudo gdebi GitHubDesktop-linux-3.2.0-linux1.deb
-echo
-
-echo "--> Installing REPO" 
-apt install repo
+echo "--> Installing Git & Repo" 
+sudo apt-get install git-all  
+sudo apt-get install repo
+sudo apt-get install git-lfs
 echo
 } 
 
@@ -56,14 +26,8 @@ echo
 Python() {
 echo "--> Installing Python 3.11" 
 sudo apt-get install python3.11
-sudo apt-get install python-is-python3
-echo 
-
-echo "--> Installing Python 3 pip" 
+sudo apt-get install python-is-python3 
 sudo apt-get install python3-pip
-echo
-
-echo "--> Installing Python Lunch" 
 pip3 install lunch
 echo 
 }
@@ -80,13 +44,26 @@ echo
 } 
 
 
+Tools() {
+echo "--> Installing MFO Choices" 
+sudo apt-get install unzip
+snap install code --classic 
+sudo apt-get install gdebi 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb 
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.2.0-linux1/GitHubDesktop-linux-3.2.0-linux1.deb
+sudo gdebi GitHubDesktop-linux-3.2.0-linux1.deb
+echo
+} 
+
+
 START=$(date +%s)
 
 Snap&Curl
-Tools
 Git&Repo
 Python
 Gradle
+Tools
 
 END=$(date +%s)
 ELAPSEDM=$(($(($END-$START))/60))
