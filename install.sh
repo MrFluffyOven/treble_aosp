5d3654ccdd08b309e7ebb8e71bf0688be067d59b
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-01-11 10:26:36 +0000

    Experimental Cleanup

--- install.sh
+++ install.sh
@@ -1,26 +1,6 @@
 ## Installer Script
 
-echo "--> Installing Git" 
-sudo apt-get install git-all 
-echo
-
-echo "--> Installing Snap" 
-sudo apt-get install snap
-echo
-
-echo "--> Installing REPO" 
-snap install repo
-echo
-
-echo "--> Installing Python 3.11" 
-sudo apt-get install python3.11
-sudo apt-get install python-is-python3
-echo
-
-echo "--> Installing JDK 11"
-sudo apt-get install openjdk-11-jdk
-echo
-
+Tools() {
 echo "--> Installing Unzip" 
 sudo apt-get install unzip
 echo
@@ -29,24 +9,30 @@ echo "--> Installing Visual Studio Code"
 snap install code --classic
 echo
 
-echo "--> Installing Gradle 7.2"
-snap install gradle --classic
+echo "--> Installing Gdebi" 
+sudo apt-get install gdebi
+echo
+
+echo "--> Installing Google Chrome" 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+echo
+}  
+
+
+Snap&Curl() {
+echo "--> Installing Snap" 
+sudo apt-get install snap
 echo
 
 echo "--> Installing Curl"
 snap install curl
-echo
+echo 
+}  
 
-echo "--> Installing Python 3 pip" 
-sudo apt-get install python3-pip
-echo
-
-echo "--> Installing Python Lunch" 
-pip3 install lunch
-echo
-
-echo "--> Installing Gdebi" 
-sudo apt-get install gdebi
+Git&Repo() {
+echo "--> Installing Git" 
+sudo apt-get install git-all 
 echo
 
 echo "--> Installing Github Desktop (Unofficial)" 
@@ -54,27 +40,45 @@ sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.2.0-li
 sudo gdebi GitHubDesktop-linux-3.2.0-linux1.deb
 echo
 
-echo "--> Installing Google Chrome" 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+echo "--> Installing REPO" 
+snap install repo
 echo
+} 
+
+
+Python() {
+echo "--> Installing Python 3.11" 
+sudo apt-get install python3.11
+sudo apt-get install python-is-python3
+echo 
+
+echo "--> Installing Python 3 pip" 
+sudo apt-get install python3-pip
+echo
+
+echo "--> Installing Python Lunch" 
+pip3 install lunch
+echo 
+}
+
+Gradle() {
+echo "--> Installing JDK 11"
+sudo apt-get install openjdk-11-jdk
+echo
+
+echo "--> Installing Gradle 7.2"
+snap install gradle --classic
+echo 
+} 
 
 
 START=$(date +%s)
 
-Git
-Snap
-REPO
+Tools
+Snap&Curl
+Git&Repo
 Python
-JDK
-CODE
 Gradle
-Curl
-Pip
-Lunch
-Gdebi
-Github
-Chrome
 
 END=$(date +%s)
 ELAPSEDM=$(($(($END-$START))/60))
